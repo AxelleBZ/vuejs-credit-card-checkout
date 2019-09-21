ef5a2cd909fa385998e8c6db5cdcf648de7216ac
AxelleBZ <axellezeller@hotmail.fr>
2019-09-20 23:04:16 -0400

    init commit

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :class="className" type="button" v-on:click="btnClick">{{ msg }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    msg: String,
+    className: String
+  },
+  // define methods under the `methods` object
+  methods: {
+    btnClick: function (event) {
+      alert('Hello !');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import './../assets/styles/button.scss';
+</style>
